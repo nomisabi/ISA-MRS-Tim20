6703ffb721d918efcd32c4ec21b4dd4b53490ea6
sabinomi <nomisabi>
2017-05-11 00:01:24 +0200

    conflicts

--- restoran/src/main/java/com/example/respository/ReservationRepository.java
+++ restoran/src/main/java/com/example/respository/ReservationRepository.java
@@ -1,5 +1,12 @@
-<<<<<<< HEAD
+
 package com.example.respository;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.domain.Reservation;
 
 import java.util.Collection;
 
@@ -22,28 +29,4 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 	public Collection<Reservation> get(Long idRestaurant, Long idTable, String dateStart, String dateEnd);
 
 }
-=======
-package com.example.respository;
 
-import java.util.Collection;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import com.example.domain.Reservation;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-
-	@Query("SELECT Object(r) FROM Reservation r WHERE r.restaurant.id = ?1")
-	public Collection<Reservation> getAllReservationOfRestaurant(Long id);
-
-	@Query("SELECT Object(r) FROM Reservation r WHERE r.restaurant.id = ?1 AND "
-			+ "((r.startTime>=?2 AND r.startTime < ?3) OR (r.endTime > ?2 AND r.endTime <= ?3))")
-	public Collection<Reservation> getAllReservationOfRestaurantInTime(Long id, String dateStart, String dateEnd);
-
-	@Query("SELECT Object(r) FROM Reservation r WHERE r.restaurant.id = ?1 AND r.table.id = ?2 AND "
-			+ "((r.startTime>=?3 AND r.startTime < ?4) OR (r.endTime > ?3 AND r.endTime <= ?4)) ")
-	public Collection<Reservation> get(Long idRestaurant, Long idTable, String dateStart, String dateEnd);
-
-}
->>>>>>> branch 'master' of https://github.com/nevenaDj/ISA-MRS-Tim20
--- restoran/src/main/java/com/example/service/ReservationServiceImp.java
+++ restoran/src/main/java/com/example/service/ReservationServiceImp.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.service;
 
 import java.util.Collection;
@@ -54,60 +53,4 @@ public class ReservationServiceImp implements ReservationService {
 		return reservationRepository.getAllReservationOfRestaurantInTime(id, dateStart, dateEnd);
 	}
 }
-=======
-package com.example.service;
 
-import java.util.Collection;
-
-import javax.persistence.LockModeType;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
-
-import com.example.domain.Reservation;
-import com.example.respository.ReservationRepository;
-
-@Service
-@Transactional(readOnly = true)
-public class ReservationServiceImp implements ReservationService {
-
-	@Autowired
-	ReservationRepository reservationRepository;
-
-	@Override
-	public Reservation getReservation(Long id) {
-		return reservationRepository.findOne(id);
-	}
-
-	@Override
-	public Collection<Reservation> getAllReservations() {
-		return reservationRepository.findAll();
-	}
-
-	@Override
-	@Transactional(readOnly = false)
-	@Lock(LockModeType.PESSIMISTIC_READ)
-	public Reservation createReservation(Reservation reservation) {
-		Assert.notNull(reservation, "Reservation could not be null.");
-		Collection<Reservation> reservations = reservationRepository.get(reservation.getRestaurant().getId(),
-				reservation.getTable().getId(), reservation.getStartTime(), reservation.getEndTime());
-		if (!reservations.isEmpty()) {
-			return null;
-		}
-		return reservationRepository.save(reservation);
-	}
-
-	@Override
-	public Collection<Reservation> getAllReservationOfRestaurant(Long idRestaurant) {
-		return reservationRepository.getAllReservationOfRestaurant(idRestaurant);
-	}
-
-	@Override
-	public Collection<Reservation> getAllReservationOfRestaurantInTime(Long id, String dateStart, String dateEnd) {
-		return reservationRepository.getAllReservationOfRestaurantInTime(id, dateStart, dateEnd);
-	}
-}
->>>>>>> branch 'master' of https://github.com/nevenaDj/ISA-MRS-Tim20
