a7b9f5a522dcd0cc5d67c66de3534e9f7c9dfcf1
sabinomi <nomisabi>
2017-05-12 01:31:39 +0200

    conflict copies

--- restoran/src/main/java/com/example/respository/GuestReservationRepository.java
+++ restoran/src/main/java/com/example/respository/GuestReservationRepository.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.example.respository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,14 +8,4 @@ import com.example.domain.GuestReservation;
 public interface GuestReservationRepository extends JpaRepository<GuestReservation, Long> {
 
 }
-=======
-package com.example.respository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.domain.GuestReservation;
-
-public interface GuestReservationRepository extends JpaRepository<GuestReservation, Long> {
-
-}
->>>>>>> branch 'master' of https://github.com/nevenaDj/ISA-MRS-Tim20
